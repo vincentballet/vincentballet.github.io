c7af2522d6eff0eb1cd292153420f290961349a3
vballet <vincent.ballet@amadeus.com>
2016-09-23 13:47:48 +0200

    new design

---
+++ js/js_home.js
@@ -0,0 +1,25 @@
+
+// var $button = $("#button");
+// var $cooltest = $("#cooltest");
+//   window.alert($button);
+
+$("#button").ready(function() {
+  $("#textbloc").velocity( { 
+    opacity: 1, 
+    top: "100%",
+    translateY: "-20px"
+  },
+  { 
+    display: "block" 
+  });
+
+    $("#logo").velocity( { 
+    opacity: 1, 
+    top: "100%",
+    height: "130",
+    width: '130'  },
+  { 
+    display: "blmeock" 
+  });
+  // $("#logo").velocity({height: "130", width: '130'});
+});
