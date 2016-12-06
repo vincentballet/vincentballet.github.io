990b5f2408e761dcfc5a67f7998131e314bf26d2
vballet <vincent.ballet@amadeus.com>
2016-12-06 14:22:14 +0100

    wait for backgruond to load before executing js

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
-$("#main").ready(function() {
-  $("#main").velocity({opacity: 1},{duration: 3000});
-  $("#text").velocity({opacity: 1},{duration: 6000});
+$(window).on("load",function() {
+  $("#main").velocity({opacity: 1},{duration: 2000});
+  $("#text").velocity({opacity: 1},{duration: 3000});
 });
 
