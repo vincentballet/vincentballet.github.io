de093bc6f939fec5ac8efa937fd1156ecd4e1d8e
Vincent Ballet <vincent.ballet@me.com>
2017-04-06 12:42:40 +0200

    playing with parameters

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 $(window).on("load",function() {
   $("#main").velocity({opacity: 1},{duration: 3000});
-  $("#text").velocity({opacity: 1},{duration: 3000});
+  $("#text").velocity({opacity: 1},{duration: 4000});
 });
 
